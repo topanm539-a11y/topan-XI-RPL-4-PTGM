d1c6dbc02d746421d00bfef998603e622f62cc7d
topanm539-a11y <topanm539@gmail.com>
2026-08-15 20:11:58 +0700

    https://onecompiler.com/python/44xtkjaj3
    try1

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 100
+
+a += 50
+print("bertambah roti:", a) 
+
+a *= 3
+print("bertambah roti:", a)
+
+a -= 10
+print("berkurang roti :", a)
+
+a /= 4
+print ("berkurang roti :", a)
+
+a += 10
+print ("bertambah roti :", a)
+print ("")
+print ("jadi hasil nya : 120.0 roti")
